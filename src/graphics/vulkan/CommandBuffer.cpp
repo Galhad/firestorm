325d5a5be352457d729f36bd44cb35a36bd06e4a
Wojciech Wilk <galhadpl@gmail.com>
2018-10-11 22:41:11 +0200

    [#9] Added basic Command Buffer with single command support
    Features:
    - begin single command
    - end single command
    - reset

---
+++ src/graphics/vulkan/CommandBuffer.cpp
@@ -0,0 +1,79 @@
+// MIT License
+//
+// Copyright (c) 2018 Wojciech Wilk
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "CommandBuffer.hpp"
+
+namespace fs::graphics
+{
+void CommandBuffer::create(const Device& device)
+{
+    this->device = &device;
+
+    VkCommandBufferAllocateInfo allocateInfo = {};
+    allocateInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+    allocateInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+    allocateInfo.commandPool = device.getCommandPool();
+    allocateInfo.commandBufferCount = 1;
+
+    vkAllocateCommandBuffers(device.getDevice(), &allocateInfo, &commandBuffer);
+}
+
+void CommandBuffer::destroy()
+{
+    vkFreeCommandBuffers(device->getDevice(), device->getCommandPool(), 1, &commandBuffer);
+    commandBuffer = VK_NULL_HANDLE;
+
+    device = nullptr;
+}
+
+void CommandBuffer::beginSingleTimeCommand()
+{
+    VkCommandBufferBeginInfo beginInfo = {};
+    beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+    beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+    vkBeginCommandBuffer(commandBuffer, &beginInfo);
+}
+
+void CommandBuffer::endSingleTimeCommand()
+{
+    vkEndCommandBuffer(commandBuffer);
+
+    VkSubmitInfo submitInfo = {};
+    submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+    submitInfo.commandBufferCount = 1;
+    submitInfo.pCommandBuffers = &commandBuffer;
+
+    vkQueueSubmit(device->getGraphicsQueue(), 1, &submitInfo, VK_NULL_HANDLE);
+    vkQueueWaitIdle(device->getGraphicsQueue());
+}
+
+const VkCommandBuffer CommandBuffer::getCommandBuffer() const
+{
+    return commandBuffer;
+}
+
+void CommandBuffer::reset()
+{
+    vkResetCommandBuffer(commandBuffer, 0);
+}
+}
---
+++ src/graphics/vulkan/CommandBuffer.hpp
@@ -0,0 +1,57 @@
+// MIT License
+//
+// Copyright (c) 2018 Wojciech Wilk
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+//furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef FIRESTORM_COMMANDBUFFER_HPP
+#define FIRESTORM_COMMANDBUFFER_HPP
+
+#include "Device.hpp"
+
+#include <memory>
+
+namespace fs::graphics
+{
+class CommandBuffer
+{
+public:
+    CommandBuffer() = default;
+    virtual ~CommandBuffer() = default;
+
+    void create(const Device& device);
+    virtual void destroy();
+
+    void beginSingleTimeCommand();
+    void endSingleTimeCommand();
+
+    void reset();
+
+    const VkCommandBuffer getCommandBuffer() const;
+
+
+private:
+    const Device* device = nullptr;
+
+    VkCommandBuffer commandBuffer = VK_NULL_HANDLE;
+};
+typedef std::unique_ptr <CommandBuffer> CommandBufferPtr;
+
+}
+#endif //FIRESTORM_COMMANDBUFFER_HPP
